12deb8473867c8fe82649e6656f17ec07b494c02
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-06-27 21:55:16 -0500

    strugling with this one

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    let ls = document.querySelector('form')
+    ls.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    create_ls(e.target.new_task_description.value)
+   ls.reset(); 
+  })
+})
+function create_ls(new_task_description){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', removeLs_item)
+  btn.textContent='x'
+  p.textContent = `${new_task_description} `;
+  p.appendChild(btn)
+  document.getElementById('box').appendChild(p)
+}
+function removeLs_item(e){
+e.target.parentNode.remove()
+}
+
